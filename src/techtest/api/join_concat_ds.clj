c08a09c2d6fbf485581b164bca4b306659b2d8d4
GenerateMe <generateme.blog@gmail.com>
2020-05-29 13:51:37 +0200

    full/semi/anti joins

--- src/techtest/api/join_concat_ds.clj
+++ src/techtest/api/join_concat_ds.clj
@@ -3,7 +3,8 @@
   (:require [tech.ml.dataset :as ds]
 
             [techtest.api.join-separate :refer [join-columns]]
-            [techtest.api.columns :refer [column-names]]))
+            [techtest.api.missing :refer [select-missing drop-missing]]
+            [techtest.api.columns :refer [column-names drop-columns]]))
 
 ;; joins
 
@@ -37,3 +38,32 @@
 (make-join-fns [[left-join ds/left-join]
                 [right-join ds/right-join]
                 [inner-join ds/inner-join]])
+
+
+(defn full-join
+  ([ds-left ds-right columns-selector] (full-join ds-left ds-right columns-selector nil))
+  ([ds-left ds-right columns-selector options]
+   (let [rj (right-join ds-left ds-right columns-selector options)]
+     (-> (->> rj
+              (ds/concat (left-join ds-left ds-right columns-selector options)))
+         (with-meta (assoc (meta rj) :name "full-join"))))))
+
+(defn semi-join
+  ([ds-left ds-right columns-selector] (semi-join ds-left ds-right columns-selector nil))
+  ([ds-left ds-right columns-selector options]
+   (let [lj (left-join ds-left ds-right columns-selector options)]
+     (-> (->> (-> lj
+                  (drop-missing)
+                  (drop-columns (vals (:right-column-names (meta lj)))))
+              (ds/unique-by identity))
+         (vary-meta assoc :name "semi-join")))))
+
+(defn anti-join
+  ([ds-left ds-right columns-selector] (anti-join ds-left ds-right columns-selector nil))
+  ([ds-left ds-right columns-selector options]
+   (let [lj (left-join ds-left ds-right columns-selector options)]
+     (-> (->> (-> lj
+                  (select-missing)
+                  (drop-columns (vals (:right-column-names (meta lj)))))
+              (ds/unique-by identity))
+         (vary-meta assoc :name "anit-join")))))
